02809999c3b5f58d7f4199cf9e6b8070664ee1e1
Andsan03 <andrewabellosanchez123456@gmail.com>
2024-11-22 13:03:17 +0100

    clase garage

--- src/org/ies/vehicles/model/Garage.java
+++ src/org/ies/vehicles/model/Garage.java
@@ -1,7 +1,62 @@
 package org.ies.vehicles.model;
 
+import java.util.Arrays;
+import java.util.Objects;
+
 public class Garage {
     private String name;
     private String direction;
     private String[] vehicles;
+
+    public Garage(String direction, String name, String[] vehicles) {
+        this.direction = direction;
+        this.name = name;
+        this.vehicles = vehicles;
+    }
+
+    public String getDirection() {
+        return direction;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String[] getVehicles() {
+        return vehicles;
+    }
+
+    public void setDirection(String direction) {
+        this.direction = direction;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setVehicles(String[] vehicles) {
+        this.vehicles = vehicles;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Garage garage = (Garage) o;
+        return Objects.equals(name, garage.name) && Objects.equals(direction, garage.direction) && Objects.deepEquals(vehicles, garage.vehicles);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, direction, Arrays.hashCode(vehicles));
+    }
+
+    @Override
+    public String toString() {
+        return "Garage{" +
+                "direction='" + direction + '\'' +
+                ", name='" + name + '\'' +
+                ", vehicles=" + Arrays.toString(vehicles) +
+                '}';
+    }
 }
